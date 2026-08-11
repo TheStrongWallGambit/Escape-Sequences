0366acb3ce937941808b63f6c96e2b72ad8f5cdf
TheStrongWallGambit <arya.chellappah@student.rjuhsd.us>
2026-08-11 13:13:33 -0700

    Create escape.java

---
+++ escape.java
@@ -0,0 +1,12 @@
+public class escape{
+
+    public static void main(String[]args){
+
+        // Escape Sequence \\ , \n, \r, \", \b
+
+    System.out.print("C:\\Users\\Docs \rFile Extension \nNote: \"Staff Don't Open\"\b\b");
+
+
+    }
+
+}
