758dcd0541b85306915b64f8523cfee3ca9a27a0
TheStrongWallGambit <arya.chellappah@student.rjuhsd.us>
2026-08-12 12:05:08 -0700

    Escape Sequence Extension
    Escape sequence Extension

--- escape.java
+++ escape.java
@@ -6,6 +6,35 @@ public class escape{
 
     System.out.println("C:\\Users\\Docs \rFile Extension \nNote: \" \tStaff Don't Open\" \b\b\b\b\b\b\b YOU");
     System.out.println("t");
+    
+    System.out.println("\"Quotes\"");
+    System.out.println("Slashes \\//");
+    System.out.println("How '\"confounding' \"\\\" it is!");
+
+    System.out.println("name\tage\theight");
+    System.out.println("Archie\t17\t5'9\"");
+    System.out.println("Betty\t17\t5'6\"");
+    System.out.println("Jughead\t16\t6'");
+
+    System.out.println("\ta\tb\tc");
+    System.out.println("\\\\");
+    System.out.println("'");
+    System.out.println("\"\"\"");
+    System.out.println("C:\nin\the downward spiral");
+
+    System.out.println("Dear \"DoubleSlash\" magazine,");
+    System.out.println();
+    System.out.println("\tYour publication confuses me. Is it");
+    System.out.println("a \\\\ slash or a //// slash?");
+    System.out.println("\nSincerely,");
+    System.out.println("Susan \"Suzy\" Smith");
+
+    System.out.println("\"Several slashes are sometimes seen,\"");
+    System.out.println("said Sally. \"I've said so.\" See?");
+    System.out.println("\\ / \\\\ // \\\\\\ ///");
+
+
+
 
     }
 
