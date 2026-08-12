804d98cb9e1ad3e29f0b806dc8c5ed89ae3c4eb2
TheStrongWallGambit <arya.chellappah@student.rjuhsd.us>
2026-08-12 11:39:19 -0700

    Refinement
    /t and /b

--- escape.java
+++ escape.java
@@ -2,10 +2,10 @@ public class escape{
 
     public static void main(String[]args){
 
-        // Escape Sequence \\ , \n, \r, \", \b
+        // Escape Sequence \\ , \n, \r, \", \b, \t
 
-    System.out.print("C:\\Users\\Docs \rFile Extension \nNote: \"Staff Don't Open\"\b\b");
-
+    System.out.println("C:\\Users\\Docs \rFile Extension \nNote: \" \tStaff Don't Open\" \b\b\b\b\b\b\b YOU");
+    System.out.println("t");
 
     }
 
